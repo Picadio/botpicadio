e588cefe53b94ff1cd8b0211f7fb939841833a03
Picadio <56791168+Picadio@users.noreply.github.com>
2020-08-21 00:19:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,119 @@ Bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print("Bot is online")
     
+@Bot.command(pass_context=True)
+async def create(ctx):
+	if str(ctx.message.channel) == "админские-настройки" or str(ctx.message.channel) == "играть-с-ботом":
+		b=str(ctx.message.author.id)
+		conn = sqlite3.connect("twobase.sqlite")
+		cursor = conn.cursor()
+		try:
+			cursor.execute('''CREATE TABLE test (id text, cm integer)''')
+		except:
+			pass
+		cursor.execute('SELECT * FROM test')
+		row = cursor.fetchone()
+		q=bool(1)
+		while row is not None:
+			if str(ctx.message.author.id) in row:
+				await ctx.message.channel.send("Вы уже создали себя")
+				q=bool(0)
+				break
+			row = cursor.fetchone()
+		if q==1:
+			cursor.execute('''INSERT INTO test (id,cm) VALUES (?,0)''', (b,))
+			conn.commit()
+			await ctx.message.channel.send("Создание выполнено упешно")
+		cursor.close()
+		conn.close()
 
+@Bot.command(pass_context=True)
+async def upg(ctx):
+	if str(ctx.message.channel) == "админские-настройки" or str(ctx.message.channel) == "играть-с-ботом":
+		print(1)
+		conn1 = sqlite3.connect("twobase.sqlite")
+		cursor1 = conn1.cursor()
+		cursor1.execute('SELECT * FROM test')
+		row1 = cursor1.fetchone()
+		w1=bool(1)
+		while row1 is not None:
+			if str(ctx.message.author.id) in row1:
+				size = int(row1[1])
+				w1=bool(0)
+				break
+		cursor1.close()
+		conn1.close()
+		if w1:
+			await ctx.message.channel.send("Вы не создали себя. Пропишите !create")
+		else:
+			conn = sqlite3.connect("mybase.sqlite")
+			cursor = conn.cursor()
+			cursor.execute('SELECT * FROM test')
+			row = cursor.fetchone()
+			w=bool(0)
+			print(2)
+			print(row[0])
+			print(ctx.message.author.id)
+			while row is not None:
+				
+				if str(ctx.message.author.id) in row:
+					w=bool(1)
+					print(3)
+					if row[1] >= 100:
+						cursor.execute('''UPDATE test SET bal=? WHERE id=?''',((row[1]-100),ctx.message.author.id,))
+						conn.commit()
+						print(3)
+						r = random.randint(0, int(size/10+1))
+						conn1 = sqlite3.connect("twobase.sqlite")
+						cursor1 = conn1.cursor()
+						cursor1.execute('SELECT * FROM test')
+						row1 = cursor1.fetchone()
+						if r >= 0 and r <= size//10:
+							print(4)
+							while row1 is not None:
+								if str(ctx.message.author.id) in row1:
+									await ctx.message.channel.send(str(ctx.message.author)+" Повезло, +1 см к бибе")
+									cursor1.execute('''UPDATE test SET cm=? WHERE id=?''',((size+1),ctx.message.author.id,))
+									conn1.commit()
+									break
+						else:
+							while row1 is not None:
+								if str(ctx.message.author.id) in row1:
+									await ctx.message.channel.send(str(ctx.message.author)+" Не повезло, -1 см от бибы")
+									cursor1.execute('''UPDATE test SET cm=? WHERE id=?''',((size-1),ctx.message.author.id,))
+									conn1.commit()
+									break
+					else:
+						await ctx.message.channel.send("Это действие стоит 100. Вам не хватает {} балов".format(100-row[1]))
+					break
+				
+			if w==0:
+				await ctx.message.channel.send("Вы не зарегистрированы. Пропишите !create")
+		cursor.close()
+		conn.close()
+@Bot.command(pass_context=True)
+async def size(ctx):
+    conn = sqlite3.connect("twobase.sqlite")
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM test')
+    row = cursor.fetchone()
+    y=bool(0)
+    while row is not None:
+        if str(ctx.message.author.id) in row:
+            y=bool(1)
+        row = cursor.fetchone()
+    if y==1:
+        cursor.execute('SELECT * FROM test')
+        row = cursor.fetchone()
+        while row is not None:
+            if str(ctx.message.author.id) in row:
+                await ctx.message.channel.send("Длина бибы:"+str(row[1]))
+                break
+            row = cursor.fetchone()
+    else:
+        await ctx.message.channel.send("Вы не создали себя. Пропишите !create")
+    cursor.close()
+    conn.close()
       
 @Bot.command(pass_context=True)
 async def game(ctx):
