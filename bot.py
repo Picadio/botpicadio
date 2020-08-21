8b87dc290aa94273ef8814ec5c2fa105abaf35e3
Picadio <56791168+Picadio@users.noreply.github.com>
2020-08-21 15:58:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -104,7 +104,7 @@ async def playcasino(ctx, color, stavka):
 				
 @Bot.command(pass_context=True)
 async def table_bal(ctx):
-	print(ctx.message.channel.roles)
+	print(ctx.message.channel.role)
 	
 @Bot.command(pass_context=True)
 async def create(ctx):
