f5dc8194574526560de28613337eda3ede211169
Picadio <56791168+Picadio@users.noreply.github.com>
2021-06-09 00:25:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -124,7 +124,7 @@ async def table_bal(ctx):
 		for line in f:
 			if line.id == row[0]:
 				await ctx.message.channel.send(str(line.name) + " : " + str(row[1]))
-			print(str(line.id)+" "+row[0])
+			
 		row = cursor.fetchone()
 	
 		
@@ -153,7 +153,7 @@ async def table_size(ctx):
 	while row is not None:
 		f = ctx.message.channel.members
 		for line in f:
-			if line.id == row[0]:
+			if line.id in row:
 				await ctx.message.channel.send(str(line.name) + " : " + str(row[2]) + " cm")
 		row = cursor.fetchone()
 	
