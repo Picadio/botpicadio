56d37813080d2f235b6c75b7ac0240cb749b7bfb
Picadio <56791168+Picadio@users.noreply.github.com>
2021-06-17 15:38:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -171,7 +171,7 @@ async def table_size(ctx):
 		if len(s) > i:
 			for line in f:
 				if line.id == s[i][0]:
-					strin += str(line.name)+" : "+str(s[i][1])+"\n"
+					strin += "💠 "+str(line.name)+" : "+str(s[i][1])+" CM\n"
 		else:
 			strin += "None"+"\n"
 	emb=discord.Embed(title="Топ по бибе", colour= 0x39d0d6, description = strin)
